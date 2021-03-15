b76c7953c0c1c0af8ce06ca03f1e09a4049f0d47
Spatay2000 <Spatay.148@gmail.com>
2021-03-15 15:32:45 +0600

    Spring Project 1 midterm

---
+++ src/main/java/dl/iitu/kz/UserDAO.java
@@ -0,0 +1,77 @@
+package dl.iitu.kz;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+    private static int PEOPLE_COUNT;
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/SpringProject";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "12345";
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public List<Person> index() {
+        List<Person> people = new ArrayList();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()) {
+                Person person = new Person();
+
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setEmail(resultSet.getString("email"));
+                person.setAge(resultSet.getInt("age"));
+
+                people.add(person);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return people;
+    }
+
+    public Person show(int id) {
+//        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        return null;
+    }
+
+    public void save(Person person) {
+//        person.setId(++PEOPLE_COUNT);
+//        people.add(person);
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() +
+                    "'," + person.getAge() + ",'" + person.getCountry() + "')";
+
+            statement.executeUpdate(SQL);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+    }
+}
